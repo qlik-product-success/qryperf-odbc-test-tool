e23623003e2b03224db2e8cdc15ad419865fc25c
Renu Lalwani <46944239+renulalwani@users.noreply.github.com>
2024-11-11 15:46:14 +0530

    Add files via upload

---
+++ qryperf.sql
@@ -0,0 +1,23 @@
+﻿@defaultDsn=sqlserver
+@hashRate=10000
+@throughputReportPeriod=10
+@maxRows=1000000
+
+@info=
+Example of a simple qryperf scripts file hello.sql 
+
+Run with:
+$ qryperf run mode=count
+$ qryperf run mode=csv > out.txt
+$ qryperf multirun mode=csv > out.txt
+
+@test1.info=Optional description
+@test2.mode=csv
+@test1=SELECT * from [TestDB].[dbo].[worldcities];
+
+
+@test2.info=Optional description
+@test2.mode=csv
+@test2=SELECT * from [TestDB].[dbo].[covid19data1M];
+
+ 
